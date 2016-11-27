e9075580b85d05af1ef63a2758dfba9ac5d9beea
lhas <luizhrqas@gmail.com>
2016-11-27 06:09:10 -0200

    Removido blocos de texto do back-end #2

--- admin/partials/tabs/output-options.php
+++ admin/partials/tabs/output-options.php
@@ -11,13 +11,3 @@
         <option value="down" <?php if($badge_position == 'down') { ?>selected="selected"<?php } ?>><?php _e('Down', $this->plugin_name); ?></option>
     </select>
 </fieldset>
-
-<fieldset>
-    <p><?php _e('First Block Text', $this->plugin_name); ?></p>
-    <textarea class="regular-text" cols="40" id="<?php echo $this->plugin_name; ?>-first_block_text" name="<?php echo $this->plugin_name; ?>[first_block_text]" value=""><?php if(!empty($first_block_text)) echo $first_block_text; ?></textarea>
-</fieldset>
-
-<fieldset>
-    <p><?php _e('Second Block Text', $this->plugin_name); ?></p>
-    <textarea class="regular-text" cols="40" id="<?php echo $this->plugin_name; ?>-second_block_text" name="<?php echo $this->plugin_name; ?>[second_block_text]" value=""><?php if(!empty($second_block_text)) echo $second_block_text; ?></textarea>
-</fieldset>
